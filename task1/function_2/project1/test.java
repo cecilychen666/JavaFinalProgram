ea05a94323f9d71ebd01417883df60947c28250b
韩祎然 <hyr_07@sina.com>
2020-12-31 09:14:03 +0800

    保存每次对象文件夹的历史版本

--- task1/function_2/project1/test.java
+++ task1/function_2/project1/test.java
@@ -10,6 +10,7 @@ public class test {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        //这里显示hashmap信息只是为了方便自己开发，非必要功能，开发完成后可删除
         System.out.println("========下面显示HashMap信息:========\n" + hash.hashADirectory.hashMap + "\n");
     }
 }
--- task2/project1/formTree.java
+++ task2/project1/formTree.java
@@ -24,7 +24,7 @@ public class formTree extends Repository {
 
                 newblob.content();                               //用于显示blob的内容，即Value值
 
-                Repository.writeFile(newblob.getKey(), newblob.getContent());//写入文件夹
+                //Repository.writeFile(newblob.getKey(), newblob.getContent());//写入文件夹
                 //存入HashMap中
                 hash.hashADirectory.hashMap.put(newblob.getKey(), newblob.getContent());//左边是文件的键：哈希值；右边是文件的值：文件内容
                 System.out.println(newblob.getKey());
@@ -41,7 +41,7 @@ public class formTree extends Repository {
                 //便于理解，方便直接把commitkey赋值给commit的key
                 willCommitKey = newtree.key;
 
-                Repository.writeFile(newtree.getKey(), newtree.getContent());
+                //Repository.writeFile(newtree.getKey(), newtree.getContent());
                 //存入HashMap中
                 hash.hashADirectory.hashMap.put(newtree.key, newtree.content);     //将文件夹的键值对放入HashMap内
 
